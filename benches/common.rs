9e526879198f8e05e0c13509a78ccbe2d97fcfdd
Avery Wagar <ajmw.subs@gmail.com>
2019-06-22 19:46:08 -0700

    Add test/benchmarks

---
+++ benches/common.rs
@@ -0,0 +1,19 @@
+#![feature(test)]
+extern crate test;
+
+use test::{Bencher, black_box};
+use merino::*;
+
+#[bench]
+fn bench_pow(b: &mut Bencher) {
+    // Optionally include some setup
+    let x: f64 = 211.0 * 11.0;
+    let y: f64 = 301.0 * 103.0;
+
+    b.iter(|| {
+        // Inner closure, the actual test
+        for i in 1..100 {
+            black_box(x.powf(y).powf(x));
+        }
+    });
+}
---
+++ tests/common.rs
@@ -0,0 +1,11 @@
+#![feature(test)]
+extern crate test;
+
+use merino::*;
+
+#[test]
+/// Can we crate a new `Merino` instance
+fn merino_contructor() {
+    assert!(Merino::new(1080, "127.0.0.1", Vec::new(), Vec::new()).is_ok())
+}
+
