bc3b60b9991d13cfcc4598014cbe2031d66a8abf
Dean Li <deantvv@gmail.com>
2021-07-14 18:43:33 +0800

    add comment to rfc doc

--- src/lib.rs
+++ src/lib.rs
@@ -50,8 +50,11 @@ enum ResponseCode {
 /// DST.addr variant types
 #[derive(PartialEq)]
 enum AddrType {
+    /// IP V4 address: X'01'
     V4 = 0x01,
+    /// DOMAINNAME: X'03'
     Domain = 0x03,
+    /// IP V6 address: X'04'
     V6 = 0x04,
 }
 
@@ -359,6 +362,37 @@ impl SOCKClient {
 
                 trace!("Connected!");
 
+                // From rfc 1928 (S6),
+                // the server evaluates the request, and returns a reply formed as follows:
+                //
+                //    +----+-----+-------+------+----------+----------+
+                //    |VER | REP |  RSV  | ATYP | BND.ADDR | BND.PORT |
+                //    +----+-----+-------+------+----------+----------+
+                //    | 1  |  1  | X'00' |  1   | Variable |    2     |
+                //    +----+-----+-------+------+----------+----------+
+                //
+                // Where:
+                //
+                //      o  VER    protocol version: X'05'
+                //      o  REP    Reply field:
+                //         o  X'00' succeeded
+                //         o  X'01' general SOCKS server failure
+                //         o  X'02' connection not allowed by ruleset
+                //         o  X'03' Network unreachable
+                //         o  X'04' Host unreachable
+                //         o  X'05' Connection refused
+                //         o  X'06' TTL expired
+                //         o  X'07' Command not supported
+                //         o  X'08' Address type not supported
+                //         o  X'09' to X'FF' unassigned
+                //      o  RSV    RESERVED
+                //      o  ATYP   address type of following address
+                //         o  IP V4 address: X'01'
+                //         o  DOMAINNAME: X'03'
+                //         o  IP V6 address: X'04'
+                //      o  BND.ADDR       server bound address
+                //      o  BND.PORT       server bound port in network octet order
+                //
                 self.stream
                     .write_all(&[
                         SOCKS_VERSION,
@@ -495,12 +529,35 @@ struct SOCKSReq {
     pub command: SockCommand,
     pub addr_type: AddrType,
     pub addr: Vec<u8>,
-    pub port: u16
+    pub port: u16,
 }
 
 impl SOCKSReq {
     /// Parse a SOCKS Req from a TcpStream
     fn from_stream(stream: &mut TcpStream) -> Result<Self, Box<dyn Error>> {
+        // From rfc 1928 (S4), the SOCKS request is formed as follows:
+        //
+        //    +----+-----+-------+------+----------+----------+
+        //    |VER | CMD |  RSV  | ATYP | DST.ADDR | DST.PORT |
+        //    +----+-----+-------+------+----------+----------+
+        //    | 1  |  1  | X'00' |  1   | Variable |    2     |
+        //    +----+-----+-------+------+----------+----------+
+        //
+        // Where:
+        //
+        //      o  VER    protocol version: X'05'
+        //      o  CMD
+        //         o  CONNECT X'01'
+        //         o  BIND X'02'
+        //         o  UDP ASSOCIATE X'03'
+        //      o  RSV    RESERVED
+        //      o  ATYP   address type of following address
+        //         o  IP V4 address: X'01'
+        //         o  DOMAINNAME: X'03'
+        //         o  IP V6 address: X'04'
+        //      o  DST.ADDR       desired destination address
+        //      o  DST.PORT desired destination port in network octet
+        //         order
         let mut packet = [0u8; 4];
         // Read a byte from the stream and determine the version being requested
         stream.read_exact(&mut packet)?;
